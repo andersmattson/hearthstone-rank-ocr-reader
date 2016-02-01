d729aaa5b6311f84bf976fde2dc599b1ac7ff482
Oskar Risberg <kokarn@gmail.com>
2016-02-01 14:31:27 +0100

    Add minimum matching area for 0

--- js/characterfinder.js
+++ js/characterfinder.js
@@ -83,18 +83,15 @@ CharacterFinder.prototype.onImgload = function(){
                 if( bestAnswer == null || bestAnswerValue > sum ){
 
                     // Check the minimum pixel area needed for each number
-                    /*
                     if( j === 0 && numberShapes[ i ].length < 130 ) {
                         continue;
-                    } else
-                    */
-                    if( j === 1 && numberShapes[ i ].length < 120 ){
+                    } else if( j === 1 && numberShapes[ i ].length < 120 ){
                         continue;
                     } else if( j === 2 && numberShapes[ i ].length < 140 ){
                         continue;
                     } else if( j === 3 && numberShapes[ i ].length < 170 ){
                         continue;
-                    } else if( j === 4 && numberShapes[ i ].length < 135 ){
+                    } else if( j === 4 && numberShapes[ i ].length < 140 ){
                         continue;
                     } else if( j === 5 && numberShapes[ i ].length < 170 ){
                         continue;
